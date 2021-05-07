66a25b979fd506b9d712b163f1011a0b0c6e79d8
ImDaeseong <cs93059@gmail.com>
2021-05-07 13:22:21 +0900

    webbrowser_test

---
+++ WinFormsChromeBrowser/WinFormsChromeBrowser/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace WinFormsChromeBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chromiumWebBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser
+            // 
+            this.chromiumWebBrowser.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser.Margin = new System.Windows.Forms.Padding(10);
+            this.chromiumWebBrowser.Name = "chromiumWebBrowser";
+            this.chromiumWebBrowser.Size = new System.Drawing.Size(800, 450);
+            this.chromiumWebBrowser.TabIndex = 0;
+            this.chromiumWebBrowser.LoadError += new System.EventHandler<CefSharp.LoadErrorEventArgs>(this.chromiumWebBrowser_LoadError);
+            this.chromiumWebBrowser.StatusMessage += new System.EventHandler<CefSharp.StatusMessageEventArgs>(this.chromiumWebBrowser_StatusMessage);
+            this.chromiumWebBrowser.AddressChanged += new System.EventHandler<CefSharp.AddressChangedEventArgs>(this.chromiumWebBrowser_AddressChanged);
+            this.chromiumWebBrowser.TitleChanged += new System.EventHandler<CefSharp.TitleChangedEventArgs>(this.chromiumWebBrowser_TitleChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chromiumWebBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser;
+    }
+}
+
---
+++ WinFormsChromeBrowser/WinFormsChromeBrowser/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WinFormsChromeBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //초기화
+            CefSettings settings = new CefSettings();
+            //CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            //settings.RemoteDebuggingPort = 8088;
+            //settings.CefCommandLineArgs.Add("disable-usb-keyboard-detect", "1");
+            //settings.CefCommandLineArgs.Add("persist_session_cookies", "1");
+            //settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1");
+            //settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            //settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            Cef.Initialize(settings);         
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            chromiumWebBrowser.Load("https://www.naver.com");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void chromiumWebBrowser_AddressChanged(object sender, CefSharp.AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                string sMsg = string.Format("chromiumWebBrowser_AddressChanged :{0} ", e.Address);
+                Console.WriteLine(sMsg);
+            }));            
+        }
+
+        private void chromiumWebBrowser_TitleChanged(object sender, CefSharp.TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                string sMsg = string.Format("chromiumWebBrowser_TitleChanged :{0} ", e.Title);
+                Console.WriteLine(sMsg);
+            }));            
+        }
+
+        private void chromiumWebBrowser_StatusMessage(object sender, CefSharp.StatusMessageEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                string sMsg = string.Format("chromiumWebBrowser_StatusMessage :{0} ", e.Value);
+                Console.WriteLine(sMsg);
+            }));
+        }
+
+        private void chromiumWebBrowser_LoadError(object sender, LoadErrorEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                string sMsg = string.Format("chromiumWebBrowser_LoadError :{0} ", e.ErrorText);
+                Console.WriteLine(sMsg);
+            }));
+        }
+    }
+}
