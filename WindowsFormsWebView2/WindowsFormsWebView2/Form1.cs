57910f86db6005c5c91e10c8004f3ac5747712c9
ImDaeseong <cs93059@gmail.com>
2021-05-07 13:24:39 +0900

    webbrowser_test

---
+++ WindowsFormsWebView2/WindowsFormsWebView2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsWebView2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (webView != null)
+            {
+                webView.Source = new Uri("https://www.naver.com");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (webView != null)
+            {
+                webView.Dispose();
+            }
+        }
+
+        private void webView_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            Console.WriteLine("webView_NavigationStarting");
+        }
+
+        private void webView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            Console.WriteLine("webView_NavigationCompleted");
+        }
+    }
+}
