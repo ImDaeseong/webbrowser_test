cd87c674c0b47d0103e857e5526a57258f0e470e
ImDaeseong <cs93059@gmail.com>
2021-05-07 13:23:45 +0900

    webbrowser_test

---
+++ WindowsFormsWebBrowser/WindowsFormsWebBrowser/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("https://www.naver.com");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Console.WriteLine(webBrowser.DocumentTitle);
+        }
+    }
+}
