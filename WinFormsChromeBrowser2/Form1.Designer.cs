fcd759306af1273affc1397f7fa001b96d369f4d
daesung <daesung@mediaweb.co.kr>
2024-10-12 14:59:20 +0900

    webbrowser_test

---
+++ WinFormsChromeBrowser2/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace WinFormsChromeBrowser2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            this.chromiumWebBrowser1.AddressChanged += new System.EventHandler<CefSharp.AddressChangedEventArgs>(this.chromiumWebBrowser1_AddressChanged);
+            this.chromiumWebBrowser1.TitleChanged += new System.EventHandler<CefSharp.TitleChangedEventArgs>(this.chromiumWebBrowser1_TitleChanged);
+            this.chromiumWebBrowser1.LoadError += new System.EventHandler<CefSharp.LoadErrorEventArgs>(this.chromiumWebBrowser1_LoadError);
+            this.chromiumWebBrowser1.LoadingStateChanged += new System.EventHandler<CefSharp.LoadingStateChangedEventArgs>(this.chromiumWebBrowser1_LoadingStateChanged);
+            this.chromiumWebBrowser1.StatusMessage += new System.EventHandler<CefSharp.StatusMessageEventArgs>(this.chromiumWebBrowser1_StatusMessage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+    }
+}
+
---
+++ WinFormsChromeBrowser2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WinFormsChromeBrowser2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(new CefSettings());
+        }
+
+        private void Navigate(string url)
+        {
+            if (!string.IsNullOrEmpty(url) && !url.StartsWith("http://") && !url.StartsWith("https://"))
+            {
+                url = "https://" + url;
+            }
+            chromiumWebBrowser1.Load(url);
+        }
+
+        private void GoBack()
+        {
+            if (chromiumWebBrowser1.CanGoBack)
+                chromiumWebBrowser1.Back();
+        }
+
+        private void GoForward()
+        {
+            if (chromiumWebBrowser1.CanGoForward)
+                chromiumWebBrowser1.Forward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Navigate("https://www.naver.com");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+
+            if (chromiumWebBrowser1 != null)
+            {
+                chromiumWebBrowser1.Dispose();
+            }
+        }
+
+        private void chromiumWebBrowser1_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            string sMsg = string.Format("chromiumWebBrowser1_AddressChanged :{0} ", e.Address);
+            Console.WriteLine(sMsg);
+        }
+
+        private void chromiumWebBrowser1_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            string sMsg = string.Format("chromiumWebBrowser1_TitleChanged :{0} ", e.Title);
+            Console.WriteLine(sMsg);
+        }
+
+        private void chromiumWebBrowser1_StatusMessage(object sender, StatusMessageEventArgs e)
+        {
+            string sMsg = string.Format("chromiumWebBrowser1_StatusMessage :{0} ", e.Value);
+            Console.WriteLine(sMsg);
+        }
+
+        private void chromiumWebBrowser1_LoadError(object sender, LoadErrorEventArgs e)
+        {
+            string sMsg = string.Format("chromiumWebBrowser1_LoadError :{0} ", e.ErrorText);
+            Console.WriteLine(sMsg);
+        }
+
+        private void chromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            Console.WriteLine("e.IsLoading:" + e.IsLoading);
+            
+            string sMsg = (e.IsLoading ? "Loading..." : chromiumWebBrowser1.Address);
+            Console.WriteLine(sMsg);
+
+            if (e.IsLoading == false)
+            {
+                Console.WriteLine("브라우저 로드 완료");
+            }
+        }
+       
+    }
+}
